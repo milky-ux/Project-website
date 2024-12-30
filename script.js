715ca484e72222616343b540f371d128f0400f3d
malik <malikragilsyaputra385@gmail.com>
2024-12-30 17:45:02 +0700

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-('.carousel').carousel ({
-    interval:3000,
-})
+const images = ["assets/product1.svg", "assets/product2.svg", "assets/product3.svg"];
+let currentIndex = 0;
+
+function changeImage(direction) {
+    if (direction === 'prev') {
+        currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    } else if (direction === 'next') {
+        currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    }
+    document.getElementById('productImage').src = images[currentIndex];
+}

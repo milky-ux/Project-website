bda8e47955424043b0624145a9d600860e76ee13
malik <malikragilsyaputra385@gmail.com>
2024-12-30 22:39:33 +0700

    commit

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-const images = ["assets/product1.svg", "assets/product2.svg", "assets/product3.svg"];
-let currentIndex = 0;
+const slider = document.querySelector(".slider");
+const slideItems = document.querySelectorAll(".slide");
 
-function changeImage(direction) {
-    if (direction === 'prev') {
-        currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
-    } else if (direction === 'next') {
-        currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
-    }
-    document.getElementById('productImage').src = images[currentIndex];
+let index = 0;
+let touchStartX = 0;
+let touchEndX = 0;
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+
+next.addEventListener('click', nextSlide);
+function nextSlide() {
+    index = (index + 1) % slideItems.length;
+    updateSlide();
 }
+
+prev.addEventListener('click', prevSlide);
+function prevSlide() {
+    index = (index - 1 + slideItems.length) % slideItems.length;
+    updateSlide();
+}
+
+function updateSlide() {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+slider.addEventListener("mouseenter", () => clearInterval(interval));
+slider.addEventListener("mouseleave", startSlide);
+
+slider.addEventListener("touchstart", (e) => {
+    touchStartX = e.touches[0].clientX;
+});
+
+slider.addEventListener("touchend", (e) => {
+    touchEndX = e.changedTouches[0].clientX;
+    handleSwipe();
+});
+
+function handleSwipe() {
+    const SWIPE_THRESHOLD = 100;
+
+    if (touchEndX - touchStartX > SWIPE_THRESHOLD) {
+        prev();
+    } else if (touchStartX - touchEndX > SWIPE_THRESHOLD) {
+        next();
+    }
+}
+
+startSlide();
